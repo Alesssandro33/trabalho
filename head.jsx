e43f0a22299db4682fc9900513ecfb489084f1d9
Alessandro da Silva Olegario <01815848@sempreuninassau.com.br>
2025-10-02 10:27:22 -0300

    Meusegundocommit

---
+++ head.jsx
@@ -0,0 +1,12 @@
+function Header() {
+    return (
+      <header>
+        <h1>Minha Lista de Alunos</h1>
+      </header>
+    );
+  }
+  
+  // Exportamos o componente para que ele possa ser usado em outros arquivos, como o App.jsx
+  export default Header;
+  
+  
